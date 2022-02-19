46bb7b407ee968d3a9275692e1876e4338957d68
nagarajakl0011 <nagaraja.murugan@kaaylabs.com>
2022-02-19 15:13:10 +0530

    client run sample page

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React js ,ant Design, mysql,node js ,express</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          comming soon project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
