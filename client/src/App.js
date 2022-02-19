84a878409f0f022d32fe902a3d10204333bda640
nagarajakl0011 <nagaraja.murugan@kaaylabs.com>
2022-02-19 16:18:25 +0530

    From UI design

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,48 @@
-import logo from "./logo.svg";
+import react, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [salary, setSalary] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React js ,ant Design, mysql,node js ,express</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          comming soon project
-        </a>
-      </header>
+    <div className="info">
+      <form onSubmit={console.log(name)}>
+        <label> Name </label>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label> Age </label>
+        <input
+          type="text"
+          name="Age"
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <label> Country </label>
+        <input
+          type="text"
+          name="Country"
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <label> Position </label>
+        <input
+          type="text"
+          name="Position"
+          onChange={(e) => setPosition(e.target.value)}
+        />
+        <label> Salary </label>
+        <input
+          type="text"
+          name="Salary"
+          onChange={(e) => setSalary(e.target.value)}
+        />
+        <button type="submit">Add Emp</button>
+      </form>
     </div>
   );
 }
