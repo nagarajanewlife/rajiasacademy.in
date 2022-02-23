cacf32e8d3168643310218a49d7bf2217ad93e83
nagarajakl0011 <nagaraja.murugan@kaaylabs.com>
2022-02-23 17:39:33 +0530

    server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import react, { useState } from "react";
+import axios from "axios";
+import { Table, Tag, Space } from "antd";
+import "antd/dist/antd.css";
 import "./App.css";
 
 function App() {
@@ -7,10 +10,77 @@ function App() {
   const [country, setCountry] = useState("");
   const [position, setPosition] = useState("");
   const [salary, setSalary] = useState(0);
+  const [listemp, setListemp] = useState([]);
 
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "empname",
+      key: "empname",
+    },
+    {
+      title: "Age",
+      dataIndex: "Age",
+      key: "Age",
+    },
+    {
+      title: "country",
+      dataIndex: "country",
+      key: "country",
+    },
+    {
+      title: "position",
+      dataIndex: "position",
+      key: "position",
+    },
+    {
+      title: "salary",
+      dataIndex: "salary",
+      key: "salary",
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "action",
+      render: (recard) => {
+        return (
+          <>
+            <span
+              onClick={() => {
+                onDelete(recard);
+              }}
+            >
+              Delete
+            </span>
+          </>
+        );
+      },
+    },
+  ];
+  const addEmployee = () => {
+    axios
+      .post("http://127.0.0.1:3003/create", {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        salary: salary,
+      })
+      .then(() => console.log("success"));
+  };
+  const listEmployes = () => {
+    console.log("list emp");
+    axios.get("http://127.0.0.1:3003/employes").then((response) => {
+      console.log(response.data);
+      setListemp(response.data);
+    });
+  };
+  const onDelete = (recard) => {
+    console.log(recard);
+  };
   return (
     <div className="info">
-      <form onSubmit={console.log(name)}>
+      <form onSubmit={addEmployee}>
         <label> Name </label>
         <input
           type="text"
@@ -43,6 +113,10 @@ function App() {
         />
         <button type="submit">Add Emp</button>
       </form>
+      <div className="listemp">
+        <button onClick={listEmployes}>show employes</button>
+      </div>
+      <Table dataSource={listemp} columns={columns} />;
     </div>
   );
 }
