47320ba5531f5bba80dadad5213f971b5707bea8
nagarajakl0011 <nagaraja.murugan@kaaylabs.com>
2022-02-24 11:37:16 +0530

    form submbit

--- online/client/src/App.js
+++ online/client/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react, { useState } from "react";
+import "./App.css";
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [salary, setSalary] = useState(0);
+
+  const addEmp = () => {
+    console.log(name);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="info">
+      <form onSubmit={addEmp}>
+        <label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="age"
+            placeholder="Enter age"
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="country"
+            placeholder="Enter country"
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="position"
+            placeholder="Enter position"
+            onChange={(e) => setPosition(e.target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="salary"
+            placeholder="Enter salary"
+            onChange={(e) => setSalary(e.target.value)}
+          />
+        </label>
+
+        <button type="submit"> Add Employee</button>
+      </form>
     </div>
   );
 }
