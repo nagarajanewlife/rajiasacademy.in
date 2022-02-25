692382c1470ab699a5cd579fedb48e692e74197d
nagarajakl0011 <nagaraja.murugan@kaaylabs.com>
2022-02-25 19:11:42 +0530

    update UI Client side

--- online/client/src/App.js
+++ online/client/src/App.js
@@ -1,15 +1,42 @@
-import react, { useState } from "react";
+import react, { useState, useEffect } from "react";
 import axios from "axios";
-import { Table, Tag, Space } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
+import {
+  Table,
+  Tag,
+  Space,
+  Form,
+  Input,
+  InputNumber,
+  Button,
+  Modal,
+} from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import "./App.css";
 function App() {
+  const [form] = Form.useForm();
+
   const [name, setName] = useState("");
+
   const [age, setAge] = useState(0);
   const [country, setCountry] = useState("");
   const [position, setPosition] = useState("");
   const [salary, setSalary] = useState(0);
   const [employess, setEmployess] = useState([]);
+  const [updateVisible, setUpdateVisible] = useState(false);
+  const [selectValue, setSelectValue] = useState({});
+  useEffect(() => {
+    console.log("use effect", Object.keys(selectValue).length);
+    {
+      Object.keys(selectValue).length !== 0 &&
+        form.setFieldsValue({
+          name: selectValue.data.empname,
+          age: selectValue.data.Age,
+          position: selectValue.data.position,
+          country: selectValue.data.country,
+          salary: selectValue.data.salary,
+        });
+    }
+  }, [selectValue]);
 
   const addEmp = () => {
     axios
@@ -40,7 +67,39 @@ function App() {
       );
     });
   };
+  // edit
 
+  const handleCancel = () => {
+    setUpdateVisible(false);
+  };
+  const onEdit = (data) => {
+    setUpdateVisible(true);
+    setSelectValue({ data });
+    console.log("selectValue", selectValue);
+  };
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const onFinish = (values) => {
+    console.log("values", values.user.name);
+    setName(values.user.name);
+    setAge(values.user.Age);
+  };
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+      number: "${label} is not a valid number!",
+    },
+    number: {
+      range: "${label} must be between ${min} and ${max}",
+    },
+  };
   const columns = [
     {
       title: "Emp ID",
@@ -82,7 +141,18 @@ function App() {
               onClick={() => {
                 onDelete(record.empid);
               }}
-              style={{ color: "red", cursor: "pointer", fontSize: 23 }}
+              style={{
+                color: "red",
+                cursor: "pointer",
+                fontSize: 23,
+                marginRight: 20,
+              }}
+            />
+            <EditOutlined
+              onClick={() => {
+                onEdit(record);
+              }}
+              style={{ color: "gray", cursor: "pointer", fontSize: 23 }}
             />
           </>
         );
@@ -138,6 +208,54 @@ function App() {
       </form>
       <button onClick={getEmployess}>Show Emp</button>
       <Table dataSource={employess} columns={columns} />;
+      <Modal
+        title="Update Employess Details"
+        visible={updateVisible}
+        // onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form
+          {...layout}
+          name="nest-messages"
+          form={form}
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+        >
+          <Form.Item name="name" label="Name">
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="age"
+            label="Age"
+            value={age}
+            rules={[
+              {
+                type: "number",
+                min: 0,
+                max: 99,
+              },
+            ]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item name="country" label="Country">
+            <Input />
+          </Form.Item>
+          <Form.Item name="position" label="Position">
+            <Input />
+          </Form.Item>
+          <Form.Item name="salary" label="Salary">
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <Button type="primary" htmlType="submit">
+              Update
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 }
